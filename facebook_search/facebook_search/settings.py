db88ce1505fd9a4c5a1680424b422cbb9e37c2fb
Arnav Kumar <arnavkumar24@hotmail.com>
2014-03-29 17:32:40 +0800

    Initial commit... Basic communication and App have been setup

---
+++ facebook_search/facebook_search/settings.py
@@ -0,0 +1,112 @@
+"""
+Django settings for facebook_search project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_facebook
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(4y@*$_b%0p2vchz_#2+rz08-uk8^fgzg_@xddk+951cahb4wv'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+FACEBOOK_APP_ID = '1398781910393871'
+FACEBOOK_APP_SECRET = '601b0776a562a70a23307ee2f210edff'
+FACEBOOK_SCOPE = ['publish_stream', 'read_stream']
+FACEBOOK_CANVAS_PAGE = 'https://apps.facebook.com/%s/' % FACEBOOK_APP_ID
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_facebook',
+    'search',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+
+)
+
+ROOT_URLCONF = 'facebook_search.urls'
+
+WSGI_APPLICATION = 'facebook_search.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'django_facebook.context_processors.facebook',
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    'django_facebook.auth_backends.FacebookBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
+AUTH_PROFILE_MODULE = 'django_facebook.FacebookProfile'
---
+++ facebook_search/facebook_search/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'facebook_search.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^facebook/', include('django_facebook.urls')),
+	url(r'^accounts/', include('django_facebook.auth_urls')),	
+)    
+urlpatterns += patterns(
+    'search.views',
+    url(r'^connect/$', 'connect', name='facebook_connect'),
+    url(r'^disconnect/$',
+        'disconnect', name='facebook_disconnect'),
+    url(r'^example/$', 'example', name='facebook_example'),
+    url(r'^test/$', 'test', name='test'),
+    url(r'^reindex/$', 'reindex', name='reindex'),
+)
